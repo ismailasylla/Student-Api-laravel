0f7f55ed978961e3af7ef8c2209d6502bd56304f
ismailasylla <ismailasyllaa@gmail.com>
2020-07-07 01:40:01 +0400

    fixed the big that was causing the api not to render on the client side

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,21 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Student;
-use Illuminate\Validation;
-use Illuminate\Support\Facades\Validator;
-use App\SClass;
+
 class StudentController extends Controller
 {
     public function add(Request $req){
 		$validator = Validator::make($req->all(), [
             "first_name" => "required",
             "last_name" => "required",
-            'class' => "required|numeric|exists:classes,id",
-            'date_of_birth' => "required|date",
+            'class' => "required",
+            'date_of_birth' => "required",
         ]);
 
         if ($validator->fails()) {
@@ -23,66 +19,70 @@ class StudentController extends Controller
                 'status'=>false,
                 'code'=>400,
                 'param'=>'first_name,last_name,class and date_of_birth are required.',
-                'message'=>$validator->errors(),
+                'message'=>'Please fill the all required data.',
             );
 
         }else{
-            $student=Student::createStudent($req->all());
-            $class=SClass::find($req->class);
-           if(count($class->students)<10){
-           	    if($student){
-	                $response=array(
-	                    'status'=>true,
-	                    'code'=>200,
-	                    'message'=>'student created successfully',
-	                    'data'=>$student
-	                );
-	            }else{
-
-	                $response=array(
-	                    'status'=>false,
-	                    'code'=>500,
-	                    'message'=>'student not added successfully',
-
-	                );
-	            }
-           }else{
-	           	$response=array(
-		                    'status'=>false,
-		                    'code'=>500,
-		                    'message'=>'student not added because class reach the maximum student limit',
-		                );
-           }
-
+            $student=Student::create([
+                'first_name'=>$req->first_name,
+                'last_name'=>$req->last_name,
+                'class'=>$req->class,
+                'date_of_birth'=>$req->date_of_birth,
+            ]);
+            if($student){
+                $response=array(
+                    'status'=>true,
+                    'code'=>200,
+                    'message'=>'student created successfully',
+                );
+            }else{
+               
+                $response=array(
+                    'status'=>false,
+                    'code'=>500,
+                    'message'=>'student not add successfully',
+                );
+            }
         }
         return response()->json($response);
     }
     public function edit(Request $req){
 		$validator = Validator::make($req->all(), [
             "id" => "required",
-            'class'=>'numeric',
-            'date_of_birth' => "date",
+            'class'=>'numeric'
         ]);
+
         if ($validator->fails()) {
             $response=array(
                 'status'=>false,
                 'code'=>400,
                 'param'=>'id is required. and class is integer value',
-                'message'=>$validator->errors(),
+                'message'=>'Please fill the all required feild.',
             );
 
         }else{
             $student=Student::find($req->id);
-          		if($student){
-              	$student=Student::updateStudent($req->all());
+            if($student){
+            	if($req->first_name){
+					$student->first_name=$req->first_name;
+            	}
+				if($req->last_name){
+					$student->last_name=$req->last_name;
+            	}
+            	if($req->class){
+					$student->class=$req->class;
+            	}
+            	if($req->code){
+					$student->code=$req->code;
+            	}
+            	$student->save();
                 $response=array(
                     'status'=>true,
                     'code'=>200,
                     'message'=>'student updated successfully',
-                    'data'=>$student
                 );
             }else{
-
+               
                 $response=array(
                     'status'=>false,
                     'code'=>400,
@@ -93,23 +93,11 @@ class StudentController extends Controller
         return response()->json($response);
     }
     public function list(){
-    	$students=Student::all();
-    	foreach ($students as $key => $value) {
-    		$student_class_data=$value->classData;
-    		$value->class=$student_class_data->code;
-    		$value->class_data=$student_class_data;
-    	}
-    	$response=array(
-                'status'=>true,
-                'code'=>200,
-                'message'=>'Data of all the students with class.',
-                'data'=>$students
-            );
-    	 return response()->json($response);
+    	
     }
-    public function view(Request $req){
+    public function view($id){
     	$validator = Validator::make($req->all(), [
-            "id" => "required|numeric",
+            "id" => "required",
         ]);
 
         if ($validator->fails()) {
@@ -123,8 +111,8 @@ class StudentController extends Controller
         }else{
 			$student=Student::find($req->id);
             if($student){
-            	$student_class_data=$student->classData;
-            	$student->class=$student_class_data->code;
+            	$student_class_data=$student->class;
+            	$student->class=$student_class_data;
                 $response=array(
                     'status'=>true,
                     'code'=>200,
@@ -132,7 +120,7 @@ class StudentController extends Controller
                     'data'=>$student
                 );
             }else{
-
+               
                 $response=array(
                     'status'=>false,
                     'code'=>400,
@@ -140,6 +128,5 @@ class StudentController extends Controller
                 );
             }
         }
-         return response()->json($response);
     }
 }

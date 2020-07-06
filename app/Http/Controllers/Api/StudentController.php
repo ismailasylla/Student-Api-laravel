2670091af41cc600577940b0f15cb06e858386ac
ismailasylla <ismailasyllaa@gmail.com>
2020-07-07 01:40:01 +0400

    fixed the bug that was causing the api not to render on the client side

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -11,6 +11,7 @@ use App\SClass;
 class StudentController extends Controller
 {
     public function add(Request $req){
+        $code=0;
 		$validator = Validator::make($req->all(), [
             "first_name" => "required",
             "last_name" => "required",
@@ -19,6 +20,7 @@ class StudentController extends Controller
         ]);
 
         if ($validator->fails()) {
+            $code=400;
             $response=array(
                 'status'=>false,
                 'code'=>400,
@@ -31,6 +33,7 @@ class StudentController extends Controller
             $class=SClass::where('code',$req->class)->first();
            if(count($class->students)<10){
            	    if($student){
+                    $code=200;
 	                $response=array(
 	                    'status'=>true,
 	                    'code'=>200,
@@ -38,7 +41,7 @@ class StudentController extends Controller
 	                    'data'=>$student
 	                );
 	            }else{
-	               
+	               $code=500;
 	                $response=array(
 	                    'status'=>false,
 	                    'code'=>500,
@@ -47,6 +50,7 @@ class StudentController extends Controller
 	                );
 	            }
            }else{
+            $code=500;
 	           	$response=array(
 		                    'status'=>false,
 		                    'code'=>500,
@@ -55,7 +59,8 @@ class StudentController extends Controller
            }
             
         }
-        return response()->json($response);
+        return response()->json($response,$code)->header('Content-Type', 'application/x-www-form-urlencoded')
+                  ->header('Access-Control-Allow-Origin', '*');
     }
     public function edit(Request $req){
 		$validator = Validator::make($req->all(), [
@@ -64,6 +69,7 @@ class StudentController extends Controller
             'date_of_birth' => "date",
         ]);
         if ($validator->fails()) {
+            $code=400;
             $response=array(
                 'status'=>false,
                 'code'=>400,
@@ -75,6 +81,7 @@ class StudentController extends Controller
             $student=Student::find($req->id);
           		if($student){
               	$student=Student::updateStudent($req->all());
+                $code=200;
                 $response=array(
                     'status'=>true,
                     'code'=>200,
@@ -82,7 +89,7 @@ class StudentController extends Controller
                     'data'=>$student
                 );
             }else{
-               
+               $code=400;
                 $response=array(
                     'status'=>false,
                     'code'=>400,
@@ -90,22 +97,29 @@ class StudentController extends Controller
                 );
             }
         }
-        return response()->json($response);
+        return response()->json($response)->header('Content-Type', 'application/x-www-form-urlencoded')
+                  ->header('Access-Control-Allow-Origin', '*');
     }
     public function list(){
     	$students=Student::all();
+        $students2=[];
     	foreach ($students as $key => $value) {
     		$student_class_data=$value->classData;
     		$value->class=$student_class_data->code;
     		$value->class_data=$student_class_data;
+            // $students2[$key]['id']=$value->id;
+            // $students2[$key]['first_name']=$value->first_name;
+            // $students2[$key]['first_name']=$value->first_name;
     	}
+        $code=200;
     	$response=array(
                 'status'=>true,
                 'code'=>200,
                 'message'=>'Data of all the students with class.',
                 'data'=>$students
             );
-    	 return response()->json($response);
+    	 return response()->json($students,$code)->header('Content-Type', 'application/x-www-form-urlencoded')
+                  ->header('Access-Control-Allow-Origin', '*');
     }
     public function view(Request $req){
     	$validator = Validator::make($req->all(), [
@@ -113,6 +127,7 @@ class StudentController extends Controller
         ]);
 
         if ($validator->fails()) {
+            $code=400;
             $response=array(
                 'status'=>false,
                 'code'=>400,
@@ -125,6 +140,7 @@ class StudentController extends Controller
             if($student){
             	$student_class_data=$student->classData;
             	$student->class=$student_class_data->code;
+                $code=200;
                 $response=array(
                     'status'=>true,
                     'code'=>200,
@@ -132,7 +148,7 @@ class StudentController extends Controller
                     'data'=>$student
                 );
             }else{
-               
+               $code=400;
                 $response=array(
                     'status'=>false,
                     'code'=>400,
@@ -140,6 +156,7 @@ class StudentController extends Controller
                 );
             }
         }
-         return response()->json($response);
+         return response()->json($response,$code)->header('Content-Type', 'application/x-www-form-urlencoded')
+                  ->header('Access-Control-Allow-Origin', '*');
     }
 }
